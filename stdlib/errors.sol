8cf3f5a7a3590f9df86024799fd13d76c73a1e21
Alex Beregszaszi <alex@rtfs.hu>
2020-11-12 21:49:30 +0000

    Add assembly examples

---
+++ stdlib/errors.sol
@@ -0,0 +1,28 @@
+function revert() {
+  assembly {
+    revert(0, 0)
+  }
+}
+
+function revert(string memory reason) {
+  assembly {
+    revert(add(reason, 32), mload(reason))
+  }
+}
+
+function assert(bool condition) {
+  if (!condition)
+    assembly {
+      invalid()
+    }
+}
+
+function require(bool condition) {
+  if (!condition)
+    revert();
+}
+
+function require(bool condition, string memory reason) {
+  if (!condition)
+    revert(reason);
+}
