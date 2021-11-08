aarlt/solidity

Basic nlohmann-json integration.

libevmasm/Assembly.h
@@ -28,8 +28,7 @@
 #include <libdevcore/Common.h>
 #include <libdevcore/Assertions.h>
 #include <libdevcore/SHA3.h>
-
-#include <json/json.h>
+#include <libdevcore/JSON.h>
 
 #include <iostream>
 #include <sstream>
@@ -136,7 +135,7 @@ public:
 	) const;
 
 	/// Create a JSON representation of the assembly.
-	Json::Value assemblyJSON(
+	Json assemblyJSON(
 		StringMap const& _sourceCodes = StringMap()
 	) const;
 
@@ -149,7 +148,7 @@ protected:
 	unsigned bytesRequired(unsigned subTagSize) const;
 
 private:
-	static Json::Value createJsonValue(std::string _name, int _begin, int _end, std::string _value = std::string(), std::string _jumpType = std::string());
+	static Json createJsonValue(std::string _name, int _begin, int _end, std::string _value = std::string(), std::string _jumpType = std::string());
 	static std::string toStringInHex(u256 _value);
 
 protected:

libsolidity/ast/AST.h
@@ -22,7 +22,6 @@
 
 #pragma once
 
-
 #include <libsolidity/ast/ASTForward.h>
 #include <libsolidity/parsing/Token.h>
 #include <libsolidity/ast/Types.h>
@@ -33,7 +32,7 @@
 #include <libevmasm/Instruction.h>
 
 #include <libdevcore/FixedHash.h>
-#include <json/json.h>
+#include <libdevcore/JSON.h>
 
 #include <boost/noncopyable.hpp>
 

solidity-docs/tr-turkish

Co-authored-by: Alex Beregszaszi <alex@rtfs.hu>

libevmasm/Assembly.h
@@ -29,11 +29,10 @@
 
 #include <libsolutil/Common.h>
 #include <libsolutil/Assertions.h>
+#include <libsolutil/JSON.h>
 #include <libsolutil/Keccak256.h>
 
 #include <libsolidity/interface/OptimiserSettings.h>
-
-#include <json/json.h>
 
 #include <iostream>
 #include <sstream>
@@ -154,7 +153,7 @@ public:
 	) const;
 
 	/// Create a JSON representation of the assembly.
-	Json::Value assemblyJSON(
+	Json assemblyJSON(
 		std::map<std::string, unsigned> const& _sourceIndices = std::map<std::string, unsigned>()
 	) const;
 
@@ -173,7 +172,7 @@ protected:
 	unsigned codeSize(unsigned subTagSize) const;
 
 private:
-	static Json::Value createJsonValue(
+	static Json createJsonValue(
 		std::string _name,
 		int _source,
 		int _begin,

libsolidity/ast/AST.h
@@ -32,9 +32,8 @@
 #include <liblangutil/SourceLocation.h>
 #include <libevmasm/Instruction.h>
 #include <libsolutil/FixedHash.h>
+#include <libsolutil/JSON.h>
 #include <libsolutil/LazyInit.h>
-
-#include <json/json.h>
 
 #include <range/v3/view/subrange.hpp>
 #include <range/v3/view/map.hpp>