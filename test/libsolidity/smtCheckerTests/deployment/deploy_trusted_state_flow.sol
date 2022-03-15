c51099a835abe140a30d6b658c8134747ba4c9f3
Leo Alt <leo@ethereum.org>
2022-03-15 12:40:00 +0100

    Adjust tests for nondeterminism
    more smtchecker tests

--- test/libsolidity/smtCheckerTests/deployment/deploy_trusted_state_flow.sol
+++ test/libsolidity/smtCheckerTests/deployment/deploy_trusted_state_flow.sol
@@ -17,7 +17,10 @@ contract C {
 // ====
 // SMTEngine: all
 // SMTExtCalls: trusted
+// SMTIgnoreOS: macos
 // ----
+// Warning 1218: (233-251): CHC: Error trying to invoke SMT solver.
 // Warning 4984: (47-50): CHC: Overflow (resulting value larger than 2**256 - 1) might happen here.
-// Warning 6328: (233-251): CHC: Assertion violation happens here.
+// Warning 6328: (233-251): CHC: Assertion violation might happen here.
 // Warning 2661: (47-50): BMC: Overflow (resulting value larger than 2**256 - 1) happens here.
+// Warning 4661: (233-251): BMC: Assertion violation happens here.
